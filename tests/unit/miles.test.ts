16ab6462e528cfb4581bc36835c7a5bd4c85a1a6
truds99 <dudscardoso9@gmail.com>
2024-11-14 18:19:06 -0300

    add test to get miles

--- tests/unit/miles.test.ts
+++ tests/unit/miles.test.ts
@@ -1,14 +1,11 @@
-import { generateMilesForTrip } from "services/miles-service";
+import { generateMilesForTrip, getMilesFromCode } from "services/miles-service";
 import * as milesCalculatorService from "../../src/services/miles-calculator-service";
+import * as milesRepository from "../../src/repositories/miles-repository";
 import { Trip, ServiceClass, AffiliateStatus } from "protocols";
 
-
-jest.mock("../../src/repositories/miles-repository", () => {
-    return {
-      saveMiles: jest.fn().mockResolvedValue(undefined),
-      findMiles: jest.fn().mockResolvedValue(undefined)
-    };
-}); 
+beforeEach(() => {
+    jest.clearAllMocks();
+})
 
 describe("miles-service Unit Testing", () => {
 
@@ -31,9 +28,29 @@ describe("miles-service Unit Testing", () => {
         };
         
         jest.spyOn(milesCalculatorService, "calculateMiles").mockReturnValueOnce(5970);
+        jest.spyOn(milesRepository, "findMiles").mockResolvedValueOnce(undefined);
+        jest.spyOn(milesRepository, "saveMiles").mockResolvedValueOnce(undefined);
 
         const miles = await generateMilesForTrip(trip);
 
         expect(miles).toBe(5970);
+    })
+
+    it("Should return miles from a code", async () => {
+        
+        
+        jest.spyOn(milesRepository, "findMiles").mockResolvedValueOnce({
+            id: 1,
+            code: 'test',
+            miles: 100
+        });
+        
+        const miles = await getMilesFromCode('test');
+
+        expect(miles).toEqual({
+            id: 1,
+            code: 'test',
+            miles: 100
+        });
     })
 })
